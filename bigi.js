95ea2ce142a1f085f1a28aa4eafda33409ca8f09
Zaheer Ahmed Gujjar <zaheercena@users.noreply.github.com>
2022-03-18 12:46:47 +0700

    Create bigi.js

---
+++ bigi.js
@@ -0,0 +1,100 @@
+<style>
+.oneclick-button
+    {
+        background-image: none;
+        background: #1979c3;
+        border: 1px solid #1979c3;
+        color: #fff;
+        cursor: pointer;
+        display: inline-block;
+        font-family: 'Open Sans','Helvetica Neue',Helvetica,Arial,sans-serif;
+        font-weight: 600;
+        box-sizing: border-box;
+        vertical-align: middle;
+        float: left;
+        line-height: 2.2rem;
+        padding: 14px 17px;
+        font-size: 1.8rem;
+        margin-bottom: 15px;
+        width: 49%;
+        border-radius: 3px;
+        width: 100% !important;
+        height: 50px !important;
+        border-radius: 8px !important;
+        font-weight: 700 !important;
+        font-size: 18px !important;
+        background-color: #e82e81 !important;
+        color: white !important;
+        cursor: pointer !important;
+        display: flex !important;
+        -webkit-box-align: center !important;
+        align-items: center !important;
+        -webkit-box-pack: center !important;
+        justify-content: center !important;
+        border: 1px solid #e82e81 !important;
+        outline-color: transparent !important;
+        margin-bottom: 0.5rem !important;
+        text-decoration: none !important;
+    }
+    .action,.primary
+    {
+        float: left;
+    }
+    .qp-custom-mage-design{
+      width: 45%;
+      padding:0px !important;
+      height: 80vh;
+    }
+    .qp8911_modal-body{
+      height: 100%;
+    }
+</style>
+<style>
+
+/* The qp8911_modal (background) */
+.qp8911_modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1000; /* Sit on top */
+  padding-top: 100px; /* Location of the box */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: #00000099; /* Black w/ opacity */
+}
+/* qp8911_modal Content */
+.qp8911_modal-content {
+  background-color: #fefefe;
+  margin: auto;
+  padding: 10px;
+  border: 1px solid #888;
+  width: 30%;
+}
+/* The Close Button */
+.close {
+  color: #aaaaaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+.close:hover,
+.close:focus {
+  color: #000;
+  text-decoration: none;
+  cursor: pointer;
+}
+.qp8911_modal-dialog.qp8911_modal-dialog-centered{
+   display: flex;
+}
+@media screen and (max-width: 768px) {
+    .qp8911_modal-content {
+        width: 90%;
+    }
+}
+.qp8911_modal-body {
+    overflow-y: auto;
+}
+</style>
+console.log("BiGI");
